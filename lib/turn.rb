8f09da1f0ab0b5f03b497f780ba4f5441934a459
Rafael Lopez <lpz.raf@gmail.com>
2018-07-02 01:18:02 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,13 +13,23 @@ def input_to_index(input)
 end
 
 def move(board, index, current_player = "X")
-   board[index] = current_player
+  board[index] = current_player
 end
 
 # turn method
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  
 end
+
+def valid_move?(board, index)
+  if index < 0  || index > 8
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return false
+  else
+    true
+  end
+end
+
 
